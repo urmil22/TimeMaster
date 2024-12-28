3c511853655e27548fd5a2710a2a09e869233e2a
Urmil Bhavsar <urmilbhavsar8@gmail.com>
2024-12-28 13:20:18 +0530

    initial project setup commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from "vite-plugin-static-copy"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.',
+        }
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+})
